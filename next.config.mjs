08c4990d020a154c8237512ebc516b6da472c382
mokhniuk <olegmokhniuk@gmail.com>
2024-10-31 15:04:52 +0100

    fix next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: "export", // <=== enables static exports
+  output: "export", // <=== enables static exports
   reactStrictMode: true,
+  images: {
+    loader: "akamai",
+    path: "",
+  },
+  assetPrefix: "./",
 };
 
 export default nextConfig;
